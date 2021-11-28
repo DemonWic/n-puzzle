ed5b6522b2afc20a6fb38e27166416e5e94ea8e2
DemonWic <ryzcov@gmail.com>
2021-11-28 10:36:25 +0300

    fix get_neighbors - 4x4 working (little fix tests -> cast types)

--- npuzzle.py
+++ npuzzle.py
@@ -1,20 +1,29 @@
 from copy import copy
 from heapq import heappop, heappush
 from itertools import count
+import datetime
 
 m = [[4, 6, 7],
           [8, 0, 2],
           [5, 3, 1]]
 
 goal = [[1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 0]]
+      [8, 0, 4],
+      [7, 6, 5]]
+
+
+# This puzzle is solvable
+# 4
+#  8  2  7  3
+# 15  6  0  1
+# 11 10  9  4
+# 12  5 14 13
 
 
 def get_neighbors(table, size, index):
     res = []
     indexes = [size, -size]
-    if index % size != 2:
+    if index % size != size - 1:
         indexes.append(1)
     if index % size != 0:
         indexes.append(-1)
@@ -49,34 +58,67 @@ def manhattan(src: dict, dst: dict):
         res += abs(x1 - x) + abs(y1 - y)
     return res
 
+def manhattan2(candidate, solved, size):
+    res = 0
+    for i in range(size*size):
+        if candidate[i] != 0 and candidate[i] != solved[i]:
+            ci = solved.index(candidate[i])
+            y = (i // size) - (ci // size)
+            x = (i % size) - (ci % size)
+            res += abs(y) + abs(x)
+    return res
 
 if __name__ == '__main__':
 
-    src = [4, 6, 7, 8, 0, 2, 5, 3, 1]
+    # src = [4, 6, 7, 8, 0, 2, 5, 3, 1]
+    # src = [4, 1, 2, 8, 0, 5, 6, 3, 7]
+    # src = [11, 14,  9, 13, 10,  7,  0,  5, 2, 15, 6, 12, 3,  1,  8,  4]
+    src = [8, 2, 7, 3,
+           15, 6, 0, 1,
+           11, 10, 9, 4,
+           12, 5, 14, 13]
+    # src = (8, 2, 7, 3, 15, 0, 6, 1, 11, 10, 9, 4, 12, 5, 14, 13)
+    # src = (8, 2, 7, 3, 15, 6, 9, 1, 11, 10, 0, 4, 12, 5, 14, 13)
+    # src = (8, 2, 7, 3, 15, 0, 6, 1, 11, 10, 9, 4, 12, 5, 14, 13)
     dst = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-    dst2 = (1, 2, 3, 8, 0, 4, 7, 6, 5)
-    res = get_coords(src, 3)
+    # dst2 = (1, 2, 3, 8, 0, 4, 7, 6, 5)
+    dst2 = (1, 2, 3, 4, 12, 13, 14, 5, 11, 0, 15, 6, 10, 9, 8, 7)
+    res = get_coords(src, 4)
     res2 = get_coords(dst, 3)
-    res3 = get_coords(dst2, 3)
+    res3 = get_coords(dst2, 4)
     # for num, coord in res.items():
     #     print(f"{num} {coord}")
     # print(">>>>>>>>>")
     # for num, coord in res2.items():
     #     print(f"{num} {coord}")
 
-    # dist = manhattan(res, res2)
+    # dist = manhattan(res, res3)
     # print(dist)
-    # dist2 = manhattan(res, res3)
+    # dist2 = manhattan2(src, dst2, 4)
     # print(dist2)
-    countt = count()
-    queue = [(0, next(countt), (tuple(src), 4), 0, None)]
+    # res = get_neighbors(src, 4, 6)
+    # print(res)
+    # for table, _ in res:
+    #     for idx, num in enumerate(table):
+    #         if idx % 4 == 0:
+    #             print('\n')
+    #         print(num, end=' ')
+    #     print("\n>>>>>>>>>\n")
+
+
+    # countt = count()
+    start = datetime.datetime.now()
+    queue = [(0, (tuple(src), 6), 0, None)]
     open_set = {}
     closed = {}
     cost = 1
-    size = 3
+    size = 4
+    # i = 0
     goal_coord = get_coords(dst2, size)
     while queue:
-        g, c, node_ind, node_w, parent = heappop(queue)
+        g, node_ind, node_w, parent = heappop(queue)
+        # i += 1
+        # print(i)
         node, idx = node_ind
         if node == dst2:
             print("NAIDENO!")
@@ -88,11 +130,12 @@ if __name__ == '__main__':
             print(len(res))
             for x in res:
                 print(x)
+            break
         if node in closed:
             continue
         closed[node] = parent
-        with open('log.log', 'a') as f:
-            f.write(f"g = {g}, c = {c}, node = {node}\n")
+        # with open('log.log', 'a') as f:
+        #     f.write(f"g = {g}, c = {c}, node = {node}\n")
         next_w = node_w + cost
         neighbors = get_neighbors(node, size, idx)
         for neighbor, n_idx in neighbors:
@@ -106,6 +149,10 @@ if __name__ == '__main__':
                 neigh_h = manhattan(get_coords(neighbor, size), goal_coord)
 
             open_set[neighbor] = next_w, neigh_h
-            heappush(queue, (next_w + neigh_h, next(countt), (neighbor, n_idx), next_w, node))
+            heappush(queue, (next_w + neigh_h, (neighbor, n_idx), next_w, node))
+
+    end = datetime.datetime.now()
+    print(end - start)
     print("VSE GOVNO")
+
 
--- tests.py
+++ tests.py
@@ -5,87 +5,87 @@ from npuzzle import get_neighbors
 class Test_get_neighbors(unittest.TestCase):
 
     def test_1(self):
-        in_table = tuple([4, 6, 7, 8, 0, 2, 5, 3, 1])
-        out = [([4, 6, 7, 0, 8, 2, 5, 3, 1], 3),
-               ([4, 6, 7, 8, 2, 0, 5, 3, 1], 5),
-               ([4, 0, 7, 8, 6, 2, 5, 3, 1], 1),
-               ([4, 6, 7, 8, 3, 2, 5, 0, 1], 7)]
+        in_table = tuple((4, 6, 7, 8, 0, 2, 5, 3, 1))
+        out = (((4, 6, 7, 0, 8, 2, 5, 3, 1), 3),
+               ((4, 6, 7, 8, 2, 0, 5, 3, 1), 5),
+               ((4, 0, 7, 8, 6, 2, 5, 3, 1), 1),
+               ((4, 6, 7, 8, 3, 2, 5, 0, 1), 7))
         res = get_neighbors(in_table, 3, 4)
         self.assertEqual(len(out), len(res), "Not equal count result")
         for r in res:
             self.assertIn(r, out)
 
     def test_2(self):
-        in_table = tuple([4, 6, 7, 8, 1, 2, 5, 3, 0])
-        out = [([4, 6, 7, 8, 1, 2, 5, 0, 3], 7),
-               ([4, 6, 7, 8, 1, 0, 5, 3, 2], 5)]
+        in_table = tuple((4, 6, 7, 8, 1, 2, 5, 3, 0))
+        out = (((4, 6, 7, 8, 1, 2, 5, 0, 3), 7),
+               ((4, 6, 7, 8, 1, 0, 5, 3, 2), 5))
         res = get_neighbors(in_table, 3, 8)
         self.assertEqual(len(out), len(res), "Not equal count result")
         for r in res:
             self.assertIn(r, out)
 
     def test_3(self):
-        in_table = tuple([0, 6, 7, 8, 1, 2, 5, 3, 4])
-        out = [([6, 0, 7, 8, 1, 2, 5, 3, 4], 1),
-               ([8, 6, 7, 0, 1, 2, 5, 3, 4], 3)]
+        in_table = tuple((0, 6, 7, 8, 1, 2, 5, 3, 4))
+        out = (((6, 0, 7, 8, 1, 2, 5, 3, 4), 1),
+               ((8, 6, 7, 0, 1, 2, 5, 3, 4), 3))
         res = get_neighbors(in_table, 3, 0)
         self.assertEqual(len(out), len(res))
         for r in res:
             self.assertIn(r, out)
 
     def test_4(self):
-        in_table = tuple([7, 6, 0, 8, 1, 2, 5, 3, 4])
-        out = [([7, 0, 6, 8, 1, 2, 5, 3, 4], 1),
-               ([7, 6, 2, 8, 1, 0, 5, 3, 4], 5)]
+        in_table = tuple((7, 6, 0, 8, 1, 2, 5, 3, 4))
+        out = (((7, 0, 6, 8, 1, 2, 5, 3, 4), 1),
+               ((7, 6, 2, 8, 1, 0, 5, 3, 4), 5))
         res = get_neighbors(in_table, 3, 2)
         self.assertEqual(len(out), len(res))
         for r in res:
             self.assertIn(r, out)
 
     def test_5(self):
-        in_table = tuple([7, 6, 2, 8, 1, 0, 5, 3, 4])
-        out = [([7, 6, 2, 8, 0, 1, 5, 3, 4], 4),
-               ([7, 6, 2, 8, 1, 4, 5, 3, 0], 8),
-               ([7, 6, 0, 8, 1, 2, 5, 3, 4], 2)]
+        in_table = tuple((7, 6, 2, 8, 1, 0, 5, 3, 4))
+        out = (((7, 6, 2, 8, 0, 1, 5, 3, 4), 4),
+               ((7, 6, 2, 8, 1, 4, 5, 3, 0), 8),
+               ((7, 6, 0, 8, 1, 2, 5, 3, 4), 2))
         res = get_neighbors(in_table, 3, 5)
         self.assertEqual(len(out), len(res))
         for r in res:
             self.assertIn(r, out)
 
     def test_6(self):
-        in_table = tuple([7, 0, 2, 8, 1, 6, 5, 3, 4])
-        out = [([0, 7, 2, 8, 1, 6, 5, 3, 4], 0),
-               ([7, 2, 0, 8, 1, 6, 5, 3, 4], 2),
-               ([7, 1, 2, 8, 0, 6, 5, 3, 4], 4)]
+        in_table = tuple((7, 0, 2, 8, 1, 6, 5, 3, 4))
+        out = (((0, 7, 2, 8, 1, 6, 5, 3, 4), 0),
+               ((7, 2, 0, 8, 1, 6, 5, 3, 4), 2),
+               ((7, 1, 2, 8, 0, 6, 5, 3, 4), 4))
         res = get_neighbors(in_table, 3, 1)
         self.assertEqual(len(out), len(res))
         for r in res:
             self.assertIn(r, out)
 
     def test_7(self):
-        in_table = tuple([7, 8, 2, 0, 1, 6, 5, 3, 4])
-        out = [([0, 8, 2, 7, 1, 6, 5, 3, 4], 0),
-               ([7, 8, 2, 1, 0, 6, 5, 3, 4], 4),
-               ([7, 8, 2, 5, 1, 6, 0, 3, 4], 6)]
+        in_table = tuple((7, 8, 2, 0, 1, 6, 5, 3, 4))
+        out = (((0, 8, 2, 7, 1, 6, 5, 3, 4), 0),
+               ((7, 8, 2, 1, 0, 6, 5, 3, 4), 4),
+               ((7, 8, 2, 5, 1, 6, 0, 3, 4), 6))
         res = get_neighbors(in_table, 3, 3)
         self.assertEqual(len(out), len(res))
         for r in res:
             self.assertIn(r, out)
 
     def test_8(self):
-        in_table = tuple([7, 8, 2, 5, 1, 6, 0, 3, 4])
-        out = [([7, 8, 2, 0, 1, 6, 5, 3, 4], 3),
-               ([7, 8, 2, 5, 1, 6, 3, 0, 4], 7)]
+        in_table = tuple((7, 8, 2, 5, 1, 6, 0, 3, 4))
+        out = (((7, 8, 2, 0, 1, 6, 5, 3, 4), 3),
+               ((7, 8, 2, 5, 1, 6, 3, 0, 4), 7))
         res = get_neighbors(in_table, 3, 6)
         self.assertEqual(len(out), len(res))
         for r in res:
             self.assertIn(r, out)
 
     def test_9(self):
-        in_table = tuple([7, 8, 2, 5, 1, 6, 3, 0, 4])
-        out = [([7, 8, 2, 5, 0, 6, 3, 1, 4], 4),
-               ([7, 8, 2, 5, 1, 6, 0, 3, 4], 6),
-               ([7, 8, 2, 5, 1, 6, 3, 4, 0], 8)]
+        in_table = tuple((7, 8, 2, 5, 1, 6, 3, 0, 4))
+        out = (((7, 8, 2, 5, 0, 6, 3, 1, 4), 4),
+               ((7, 8, 2, 5, 1, 6, 0, 3, 4), 6),
+               ((7, 8, 2, 5, 1, 6, 3, 4, 0), 8))
         res = get_neighbors(in_table, 3, 7)
         self.assertEqual(len(out), len(res))
         for r in res:
